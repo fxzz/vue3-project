5351ca2719ac9c144ab5052e3cdaf860354193a6
fxzz <fxzzy@naver.com>
2024-05-20 18:48:22 +0900

    todo 수정, 삭제, 모달

--- src/pages/todo/ModifyPage.vue
+++ src/pages/todo/ModifyPage.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="text-3xl font-extrabold">Todo Modify Page</div>
+  <div class="p-4 w-full bg-white">
+    <div class="text-2xl font-extrabold">Todo Modify Page</div>
+    <ModifyComponent :tno="paramTno" />
+  </div>
 </template>
-<script setup></script>
+
+<script setup>
+import ModifyComponent from "../../components/todo/ModifyComponent.vue";
+import { useRoute } from "vue-router";
+const route = useRoute();
+const paramTno = route.params.tno;
+</script>
 <style></style>
